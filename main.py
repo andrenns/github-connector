28a7a2eb144e6fbe751f9d2f5427f31b83743cef
André <andrenunes.se@gmail.com>
2020-11-23 20:14:59 +0000

    Finish docker configuration and adding ngrok

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 from flask import json
 from flask import request
 from flask import Flask
-from flask_ngrok import run_with_ngrok
 
 app = Flask(__name__)
-run_with_ngrok(app)  # Start ngrok when app is run
 
 
 @app.route('/github', methods=['POST'])
 def api_github_message():
     if request.headers['Content-type'] == 'application/json':
         github_data = json.dumps(request.json)
-        print(github_data)
         return github_data
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', debug=True)
